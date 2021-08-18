97e47751c6b82ed8327d811b9a921b6d5686d2ba
Ayushmaan sharma <55322319+ayushman999@users.noreply.github.com>
2021-08-19 02:12:51 +0530

    kruskal algorithm using unified method

--- src/DSA2/Graphs/Algos/KruskalsAlgo.java
+++ src/DSA2/Graphs/Algos/KruskalsAlgo.java
@@ -9,32 +9,78 @@ public class KruskalsAlgo {
         Scanner sc=new Scanner(System.in);
         int v=sc.nextInt();
         int e=sc.nextInt();
-        int[][] adj=new int[v][v];
         PriorityQueue<Triplets> pq=new PriorityQueue<>(new compareTriplets());
 
         for(int i=0;i<e;i++)
         {
-            boolean[] visited=new boolean[v];
             int fv=sc.nextInt();
             int lv=sc.nextInt();
             int wv=sc.nextInt();
-            adj[fv][lv] = wv;
-            adj[lv][fv] = wv;
             pq.add(new Triplets(fv, lv, wv));
         }
-        int[][] result=check(adj,pq);
-
-
+/*
+        int[][] result=check(v,pq);
+*/
+        int[][] betterResult=checkCN(v,pq);
 
     }
 
-    private static int[][] check(int[][] adj, PriorityQueue<Triplets> pq) {
-        int[][] arr=new int[adj.length][adj.length];
+    private static int[][] checkCN(int v, PriorityQueue<Triplets> pq) {
+        //union find algorithm
+        int[][] arr=new int[v][v];
+        int[] parent=new int[v];
+        for(int i=0;i<v;i++)
+        {
+            parent[i]=i;
+        }
+        while (!pq.isEmpty())
+        {
+            Triplets triplets=pq.poll();
+            if(!cyclic(triplets,parent))
+            {
+                int s=triplets.sv;
+                int e=triplets.ev;
+                int w=triplets.wv;
+                arr[s][e]=w;
+                arr[e][s]=w;
+            }
+        }
+        return arr;
+    }
+    private static boolean cyclic(Triplets triplets, int[] parent) {
+        int v1=triplets.sv;
+        int v2=triplets.ev;
+        int a=parent[v1];
+        while (a!=parent[a])
+        {
+            a=parent[a];
+        }
+        int p1=a;
+        a=parent[v2];
+        while (a!=parent[a])
+        {
+            a=parent[a];
+        }
+        int p2=a;
+        if(p1==p2)
+        {
+            return true;
+        }
+        else
+        {
+            parent[v2]=p1;
+            return false;
+        }
+
+    }
+
+    private static int[][] check(int e, PriorityQueue<Triplets> pq) {
+        int[][] arr=new int[e][e];
         int count=0;
         while (!pq.isEmpty())
         {
             Triplets t=pq.poll();
-            boolean[] visited=new boolean[adj.length];
+            boolean[] visited=new boolean[e];
             arr[t.sv][t.ev]=t.wv;
             arr[t.ev][t.sv]=t.wv;
             if(isCircular(arr,visited,-1,t.sv))
